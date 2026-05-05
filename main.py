6d5d27b9df5938e77d22524a9add0579d9a1d5cc
GaroPlay <garokes0@gmail.com>
2026-05-05 14:39:03 +0300

    Rename, add button clear.

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def ButtonClick(ButtonText):
     global Start
     global LastCommand
     global DisplayResult
+    global Result
+
+    if ButtonText == "C":
+        Start = True
+        Result = 0
+        LastCommand = "="
+        DisplayResult.configure(text=str(int(Result)))
+        return
 
     if ButtonText.isdigit() or ButtonText=='.':
         if Start:
@@ -24,14 +32,14 @@ def ButtonClick(ButtonText):
                 NumberCountForDisplay += 1
 
         if NumberCountForDisplay < 12 and (ButtonText != '.' or CurrentDisplayText.find('.') == -1):
-                DisplayResult.configure(text=(DisplayResult.cget("text")+ButtonText))
+            DisplayResult.configure(text=(DisplayResult.cget("text")+ButtonText))
     else:
         if Start:
             LastCommand=ButtonText
         else:
-           CalculateNumber(float(DisplayResult.cget("text")))
-           LastCommand = ButtonText
-           Start = True
+            CalculateNumber(float(DisplayResult.cget("text")))
+            LastCommand = ButtonText
+            Start = True
 
 def CalculateNumber(x):
     global Result
@@ -80,16 +88,22 @@ DisplayResult = tkinter.Label(WindowsProgramRoot,text="0",font=("Times New Roman
                               fg="whitesmoke",bd=15, relief="sunken",padx=20,pady=10, anchor="e")
 DisplayResult.grid(row=0, column=0, columnspan=4, sticky='nsew', padx=10, pady=10)
 
+ButtonClear = tkinter.Button(WindowsProgramRoot, text="Clear", font="Tahoma 20 bold",
+                             bg="tomato", fg="white",activebackground="red", activeforeground="white",
+                             bd=4, relief="raised",command=lambda: ButtonClick("C"))
+ButtonClear.grid(row=1, column=0, columnspan=4, padx=5, pady=5, ipadx=20, ipady=15, sticky="nsew")
+
+
 for row in range(4):
     for column in range(4):
         button = tkinter.Button(WindowsProgramRoot,text=TupleButtons[row][column],font="Tahoma 20",bg="slategray",
                                 fg="whitesmoke",activebackground = "steelblue",activeforeground="white",
                                 bd=4,relief="raised",command=lambda text = TupleButtons[row][column]:ButtonClick(text))
-        button.grid(row=row+1,column=column,padx=5,pady=5,ipadx=20,ipady=20,sticky="nsew")
+        button.grid(row=row+2,column=column,padx=5,pady=5,ipadx=20,ipady=20,sticky="nsew")
         button.bind("<Enter>", ButtonHoverEnter)
         button.bind("<Leave>", ButtonHoverLeave)
 
-for i in range(5):
+for i in range(6):
     WindowsProgramRoot.grid_rowconfigure(i, weight=1)
     if i < 4:
         WindowsProgramRoot.grid_columnconfigure(i, weight=1)
